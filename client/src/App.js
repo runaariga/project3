291915a3ab84897aaaaa3ebda622bab10d79f7d0
Kamyar Mivehchi <kammivehchi@gmail.com>
2022-03-03 20:29:03 -0800

    fix merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-import MusicBar from './components/musicBar';
-import Navbar from './components/navbar';
-import Blob from './components/blob';
-import Login from './components/login';
-import Dashboard from './components/dashboard';
-const code = new URLSearchParams(window.location.search).get('code');
-
-function App() {
-  return (
-    <div className="App min-h-screen bg-myyellow-100 ">
-      <Navbar navStyle='text-mybrown-300 font-bold' />
-      {code ? <Dashboard code={code} /> : <Login />}
-      <MusicBar divStyle='bg-mybrown-300 text-white' />
-    </div>
-  );
-=======
 import "bootstrap/dist/css/bootstrap.min.css"
 import Login from "./Login"
 import Dashboard from "./Dashboard"
@@ -25,7 +6,6 @@ const code = new URLSearchParams(window.location.search).get("code")
 
 function App() {
   return code ? <Dashboard code={code} /> : <Login />
->>>>>>> 9ed8457325d476dfa8e2355e500b42795b4fc21b
 }
 
 export default App
--- server/server.js
+++ server/server.js
@@ -1,60 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const SpotifyWebApi = require('spotify-web-api-node');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-
-const app = express();
-app.use(cors());
-app.use(bodyParser.json())
-const PORT = process.env.PORT || 3001;
-app.post('/refresh', (req, res) => {
-    const refreshToken = req.body.refresh_token
-    console.log(refreshToken)
-    const spotifyApi = new SpotifyWebApi({
-        redirectUri: 'http://localhost:3000',
-        clientId: process.env.SPOTIFY_CLIENT_ID,
-        clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-        refreshToken
-    })
-    spotifyApi.refreshAccessToken()
-        .then(data => {
-            res.json({
-                accessToken: data.body.refresh_token,
-                expiresIn: data.body.expires_in
-            })
-
-        }).catch((err) => {
-            console.log(err)
-            res.sendStatus(400)
-        })
-
-
-})
-
-
-app.post('/login', (req, res) => {
-    const code = req.body.code
-    const spotifyApi = new SpotifyWebApi({
-        redirectUri: 'http://localhost:3000',
-        clientId: '2bb574410c7f4e1bb11e1a9d4d8908a3',
-        clientSecret: '83b4ceabf72a4d3798be07a94d3e0804'
-    })
-
-    spotifyApi.authorizationCodeGrant(code).then(data => {
-        res.json({
-            accessToken: data.body.access_token,
-            refreshToken: data.body.refresh_token,
-            expiresIn: data.body.expires_in
-        })
-    }).catch((err) => {
-        console.log(err)
-        res.sendStatus(400)
-    })
-})
-
-app.listen(PORT)
-=======
 require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
@@ -119,4 +62,3 @@ app.get("/lyrics", async (req, res) => {
 })
 
 app.listen(3001)
->>>>>>> 9ed8457325d476dfa8e2355e500b42795b4fc21b
