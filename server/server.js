f38e19cf001d901a990f93768ec22ee8bf11070f
Ace <omarace95@gmail.com>
2022-03-10 19:03:36 -0800

    server code edit

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const lyricsFinder = require("lyrics-finder")
 const SpotifyWebApi = require("spotify-web-api-node")
 
 const app = express()
+const PORT = process.env.PORT || 3001
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -61,4 +62,4 @@ app.get("/lyrics", async (req, res) => {
   res.json({ lyrics })
 })
 
-app.listen(3001)
+app.listen(PORT)
